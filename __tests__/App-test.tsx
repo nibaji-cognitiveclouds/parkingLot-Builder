236a2667db6459374ed5e4d1865015c3cce63872
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-05 02:25:48 +0530

    init test with enzyme

--- __tests__/App-test.tsx
+++ __tests__/App-test.tsx
@@ -4,13 +4,25 @@
 
 import 'react-native';
 import React from 'react';
+// @ts-ignore
+import { mount, shallow } from 'enzyme';
+
 import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+import HomeScreen from '../src/HomeScreen';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-it('App renders correctly', (done) => {
-  jest.useFakeTimers();
-  renderer.create(<App />);
-  done()
-});
+describe("App", () => {
+  it('renders correctly', (done) => {
+    jest.useFakeTimers();
+    renderer.create(<App />);
+    done()
+  });
+
+  it('should contain Stack.Screen', function () {
+    const Stack = createNativeStackNavigator();
+    expect(shallow(<App />).contains(<Stack.Screen name="Home" component={HomeScreen} />)).toBe(true);
+  });
+})
--- __tests__/Home-test.js
+++ __tests__/Home-test.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+
 import HomeScreen from '../src/HomeScreen';
 
-test('Home renders correctly', (done) => {
-  jest.useFakeTimers();
-  const tree = renderer.create(<HomeScreen/>).toJSON();
-  expect(tree).toMatchSnapshot();
-  done()
-});
+describe('HomeScreen', () => {
+
+  //snapshot test
+  it('renders HomeScreen correctly', (done) => {
+    jest.useFakeTimers();
+    const tree = renderer.create(<HomeScreen/>).toJSON();
+    expect(tree).toMatchSnapshot();
+    done()
+  });
+
+})
