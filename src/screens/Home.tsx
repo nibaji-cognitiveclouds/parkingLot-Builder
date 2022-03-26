48c3cba01c1df27937539e632dfa6f80cca33e5a
Nidhun Balaji T R <nidhun.balaji@cognitiveclouds.com>
2022-03-26 22:15:22 +0530

    setup home

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,30 @@
 /** @format */
 
-import { FC } from "react";
-import { SafeAreaView } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import React, { FC, useState } from "react";
+import { Button, SafeAreaView, TextInput } from "react-native";
 
 const Home: FC = () => {
-	return <SafeAreaView testID="home"></SafeAreaView>;
+	const [lots, setLots] = useState<number>(0);
+
+	const navigation = useNavigation();
+
+	return (
+		<SafeAreaView testID="home">
+			<TextInput
+				placeholder="Enter number of Parking Lots"
+				placeholderTextColor={"grey"}
+				keyboardType="numeric"
+				onChangeText={(text) => setLots(Number(text))}
+			/>
+			<Button
+				title="Submit"
+				onPress={() => {
+					navigation.navigate("Lots", { lots: lots });
+				}}
+			/>
+		</SafeAreaView>
+	);
 };
 
 export default Home;
